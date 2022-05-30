a68692d0ae49c928198a0370b7a4ba7f750e1057
Jakob Majid <jakobmajid00@gmail.comPakistan2680git config --global user.name Jakob>
2022-05-30 11:55:35 +0200

    Færdig med rytterController

--- src/main/java/com/example/tourdefrancebackend/controller/CykelRytterController.java
+++ src/main/java/com/example/tourdefrancebackend/controller/CykelRytterController.java
@@ -4,15 +4,17 @@ import com.example.tourdefrancebackend.model.CykelRytter;
 import com.example.tourdefrancebackend.repository.CykelRytterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
 
 public class CykelRytterController {
-
+    //TODO OPRETTE, SLETTE, RETTE
     @Autowired
     CykelRytterRepository cykelRytterRepository;
 
@@ -28,6 +30,32 @@ public class CykelRytterController {
     @ResponseStatus(HttpStatus.CREATED)
     public CykelRytter postCykelRytter(@RequestBody CykelRytter cykelRytter) {
         return cykelRytterRepository.save(cykelRytter);
+    }
+
+    // Sletter cykelrytter
+    @DeleteMapping("/slette/cykelrytter{id}")
+    public ResponseEntity<String> sletteCykelRytter(@PathVariable int id) {
+        try {
+            cykelRytterRepository.deleteById(id);
+            return new ResponseEntity<>("slette id = " + id, HttpStatus.NOT_FOUND);
+        } catch (Exception err) {
+            return new ResponseEntity<>("Kunne ikke finde id = " + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Update cykelrytter
+    @PutMapping("/update/cykelrytter/{id}")
+    public ResponseEntity<CykelRytter> updateCykelRytter(@PathVariable int id, @RequestBody CykelRytter cykelRytter) {
+        cykelRytter.setRytterId(id);
+        Optional<CykelRytter> optionalCykelRytter = cykelRytterRepository.findById(id);
+        if (optionalCykelRytter.isPresent()) {
+            cykelRytterRepository.save(cykelRytter);
+            return new ResponseEntity<>(cykelRytter, HttpStatus.OK);
+        } else {
+            CykelRytter ikkeFundet = new CykelRytter();
+            ikkeFundet.setRytterNavn("Ikke fundet ved hjælp af id:" + id);
+            return new ResponseEntity<>(ikkeFundet, HttpStatus.NOT_FOUND);
+        }
     }
 
 
